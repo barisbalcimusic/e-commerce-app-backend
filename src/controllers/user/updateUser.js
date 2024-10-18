2d60eb736bc71f9a379940575b56005bf6fbf98a
Baris Balci <barisbalcimusic@gmail.com>
2024-10-19 00:45:58 +0200

    updatedUser controller fixed

--- src/controllers/user/updateUser.js
+++ src/controllers/user/updateUser.js
@@ -1,8 +1,20 @@
+import { pool } from "../../utils/config/DBconfig.js";
+
 export const updateUser = async (req, res, next) => {
   try {
-    const { fieldToUpdate } = req.body;
+    const { fieldToEdit, value, userId } = req.body;
 
-    res.status(200).json({ message: `${fieldToUpdate} has been updated.` });
+    const allowedFields = ["firstname", "lastname", "password"];
+
+    if (!allowedFields.includes(fieldToEdit)) {
+      return res.status(400).json({ message: "Invalid field to update." });
+    }
+
+    const query = `UPDATE users SET ${fieldToEdit} = ? WHERE user_id = ?`;
+
+    await pool.execute(query, [value, userId]);
+
+    res.status(200).json({ message: `${fieldToEdit} has been updated.` });
   } catch (error) {
     next(error);
   }
