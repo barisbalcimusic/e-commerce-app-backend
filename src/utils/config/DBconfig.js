919daff04b87753a6ce2db60eb4c7c301c24c8d4
Baris Balci <barisbalcimusic@gmail.com>
2024-11-15 16:23:32 +0100

    initializing  and seeding queries added

--- src/utils/config/DBconfig.js
+++ src/utils/config/DBconfig.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2/promise";
 import { configDotenv } from "dotenv";
+import fs from "fs";
 
 configDotenv();
 
@@ -22,3 +23,89 @@ export const pool = mysql.createPool({
   keepAliveInitialDelay: 0,
   multipleStatements: true,
 });
+
+// INITIALIZE THE DATABASE
+const initializeDatabase = async () => {
+  try {
+    const initQuery = fs.readFileSync("src/utils/config/init.sql", "utf-8");
+    await pool.query(initQuery);
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Database initialization failed: ", error.message);
+  }
+};
+
+// SEED DATABASE
+const seedDatabase = async () => {
+  try {
+    const jsonData = JSON.parse(fs.readFileSync("products.json", "utf-8"));
+    for (const product of jsonData) {
+      // INSERT PRODUCTS
+      const productQuery =
+        "INSERT INTO products (id, name, description, category, targetGroup, brand, price, rating, discountPercentage, stock, soldCount) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+      await pool.query(productQuery, [
+        product.id,
+        product.name,
+        product.description,
+        product.category,
+        product.targetGroup,
+        product.brand,
+        product.price,
+        product.rating,
+        product.discountPercentage,
+        product.stock,
+        product.soldCount,
+      ]);
+
+      // INSERT SIZES
+      for (const size of product.sizes) {
+        const isAvailable = product.availableSizes.includes(size);
+        const sizeQuery =
+          "INSERT INTO sizes (product_id, size, isAvailable) VALUES (?, ?, ?)";
+        await pool.query(sizeQuery, [product.id, size, isAvailable]);
+      }
+
+      //INSERT COLORS
+      for (const color of product.colors) {
+        const colorQuery =
+          "INSERT INTO colors (product_id, name, hexCode) VALUES (?, ?, ?)";
+        await pool.query(colorQuery, [product.id, color.name, color.hexCode]);
+      }
+
+      // INSERT IMAGES
+      for (const image of product.images) {
+        const imageQuery =
+          "INSERT INTO images (product_id, url, alt) VALUES (?, ?, ?)";
+        await pool.query(imageQuery, [product.id, image.url, image.alt]);
+      }
+
+      // INSERT MATERIALS
+      for (const material of product.materials) {
+        const materialQuery = `INSERT INTO materials (product_id, name, percentage) VALUES (?, ?, ?)`;
+        await pool.query(materialQuery, [
+          product.id,
+          material.name,
+          material.percentage,
+        ]);
+      }
+
+      // INSERT CARE INSTRUCTIONS
+      for (const instruction of product.careInstructions) {
+        const careQuery = `INSERT INTO care_instructions (product_id, instruction) VALUES (?, ?)`;
+        await pool.query(careQuery, [product.id, instruction]);
+      }
+    }
+
+    console.log("Database seeding successfull");
+  } catch (error) {
+    console.error("Database seeding failed: ", error.message);
+  }
+};
+
+const queryFunctions = async () => {
+  await initializeDatabase();
+  await seedDatabase();
+  await pool.end();
+};
+
+queryFunctions();
--- src/utils/config/init.sql
+++ src/utils/config/init.sql
@@ -1,14 +1,63 @@
-CREATE TABLE IF NOT EXISTS baro (
+-- CREATE DATABASE IF NOT EXISTS
+CREATE DATABASE IF NOT EXISTS `e-commerce`;
+
+USE `e-commerce`;
+
+-- PRODUCTS
+CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    description VARCHAR(255),
-    price DECIMAL(10, 2),
-    discountPercentage DECIMAL(5, 2),
-    rating DECIMAL(3, 2),
-    stock INT,
-    brand VARCHAR(255),
+    name VARCHAR(255),
+    description TEXT,
     category VARCHAR(255),
-    thumbnail VARCHAR(255),
-    soldAmount INT
+    targetGroup VARCHAR(255),
+    brand VARCHAR(255),
+    price DECIMAL(10, 2),
+    rating DECIMAL(3, 2),
+    discountPercentage DECIMAL(5, 2),
+    stock INT,
+    soldCount INT 
 );
 
+-- SIZES
+CREATE TABLE IF NOT EXISTS sizes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    size VARCHAR(50),
+    isAvailable BOOLEAN,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+-- COLORS
+CREATE TABLE IF NOT EXISTS colors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    name VARCHAR(50),
+    hexCode VARCHAR(7),
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+-- IMAGES
+CREATE TABLE IF NOT EXISTS images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    url VARCHAR(255),
+    alt TEXT,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+-- MATERIALS
+CREATE TABLE IF NOT EXISTS materials (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    name VARCHAR(255),
+    percentage DECIMAL(5, 2),
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+-- CARE INSTRUCTIONS
+CREATE TABLE IF NOT EXISTS care_instructions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    instruction TEXT,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
