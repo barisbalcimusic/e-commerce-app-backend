4e59ffbb83fd4bc644fc4162d0dbcb5fee9acf62
Baris Balci <barisbalcimusic@gmail.com>
2024-11-15 17:53:18 +0100

    getDetailsOfProduct created as dynamic controller

---
+++ src/controllers/products/getDetailsOfProduct.js
@@ -0,0 +1,35 @@
+import { pool } from "../../utils/config/DBconfig.js";
+
+export const getDetailsOfProduct = async (req, res, next) => {
+  const { detail, id } = req.params;
+
+  console.log(detail, id);
+
+  if (!detail || !id) {
+    return res
+      .status(400)
+      .json({ message: "Missing detail or id information" });
+  }
+
+  const allowedDetails = [
+    "sizes",
+    "colors",
+    "images",
+    "materials",
+    "care_instructions",
+  ];
+
+  if (!allowedDetails.includes(detail)) {
+    return res.status(400).json({ message: "Invalid detail type" });
+  }
+
+  try {
+    const [data] = await pool.execute(
+      `SELECT * FROM ${detail} WHERE product_id = ?`,
+      [id]
+    );
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import { getAllProducts } from "../controllers/products/getAllProducts.js";
 import { deleteAllProducts } from "../controllers/products/deleteAllProducts.js";
 import { getSingleProduct } from "../controllers/products/getSingleProduct.js";
-import { getSizesOfSingleProduct } from "../controllers/products/getSizesOfSingleProduct.js";
+import { getDetailsOfProduct } from "../controllers/products/getDetailsOfProduct.js";
 
 export const productRouter = express.Router();
 
@@ -12,5 +12,5 @@ productRouter.route("/").get(getAllProducts).delete(deleteAllProducts);
 // SINGLE PRODUCT
 productRouter.route("/:id").get(getSingleProduct);
 
-// SIZES OF A SINGLE PRODUCT
-productRouter.route("/:id/sizes").get(getSizesOfSingleProduct);
+// DETAILS OF A SINGLE PRODUCT (DYNAMIC)
+productRouter.route("/:id/:detail").get(getDetailsOfProduct);
