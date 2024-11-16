31cff43c1d61737ab105d896ea333220540c8649
Baris Balci <barisbalcimusic@gmail.com>
2024-11-16 14:41:50 +0100

    query logic improved in getFilteredProducts controller

--- src/controllers/products/getFilteredProducts.js
+++ src/controllers/products/getFilteredProducts.js
@@ -3,7 +3,7 @@ import { pool } from "../../utils/config/DBconfig.js";
 export const getFilteredProducts = async (req, res, next) => {
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
-  const documentIndex = (page - 1) * limit;
+  const offset = (page - 1) * limit;
 
   const { targetGroup, subCategory } = req.query;
 
@@ -11,22 +11,47 @@ export const getFilteredProducts = async (req, res, next) => {
   const queryParams = [];
 
   if (targetGroup) {
-    whereClause += " AND targetGroup = ?";
+    whereClause += " AND products.targetGroup = ?";
     queryParams.push(targetGroup);
   }
 
   if (subCategory) {
-    whereClause += " AND category = ?";
+    whereClause += " AND products.category = ?";
     queryParams.push(subCategory);
   }
 
   try {
-    const [data] = await pool.execute(
-      `SELECT * FROM products WHERE 1=1 ${whereClause} LIMIT ? OFFSET ?`,
-      [...queryParams, String(limit), String(documentIndex)]
-    );
+    const query = `
+      SELECT 
+        products.*,
+        JSON_ARRAYAGG(
+          JSON_OBJECT('url', images.url, 'alt', images.alt)
+        ) AS images
+      FROM products
+      LEFT JOIN images ON products.id = images.product_id
+      WHERE 1=1 ${whereClause}
+      GROUP BY 
+        products.id, 
+        products.name, 
+        products.description, 
+        products.category, 
+        products.targetGroup, 
+        products.brand, 
+        products.price, 
+        products.rating, 
+        products.discountPercentage, 
+        products.stock, 
+        products.soldCount
+      ORDER BY products.soldCount DESC
+      LIMIT ? OFFSET ?`;
+
+    queryParams.push(String(limit), String(offset));
+
+    const [data] = await pool.execute(query, queryParams);
+
     res.status(200).json(data);
   } catch (error) {
+    console.error("Database Query Error:", error.message);
     next(error);
   }
 };
