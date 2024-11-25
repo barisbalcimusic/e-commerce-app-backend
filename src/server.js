31c8f9235962ff3ebdcc0dcdc6204063a6190329
Baris Balci <barisbalcimusic@gmail.com>
2024-11-25 11:33:20 +0100

    cors deactivated

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express();
 dotenv.config({ path: "../.env" });
 const port = process.env.PORT;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 app.use(express.json());
 
 app.use(cookieParser());
