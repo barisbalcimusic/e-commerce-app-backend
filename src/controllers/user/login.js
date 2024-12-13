ed972e8f85bef8ac0be94b0ba9d50667e16f842f
Baris Balci <barisbalcimusic@gmail.com>
2024-12-13 19:30:38 +0100

    error handlings updated

--- src/controllers/user/login.js
+++ src/controllers/user/login.js
@@ -11,32 +11,32 @@ export const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
+    if (!email || !password) {
+      return res.status(400).json({ message: "missingInput" });
+    }
+
     const [[user]] = await pool.execute("SELECT * FROM users WHERE email = ?", [
       email,
     ]);
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: "userNotFound" });
     }
 
     const isPasswordCorrect = await comparePasswords(password, user.password);
 
     if (!isPasswordCorrect) {
-      return res.status(401).json({ message: "Invalid credentials" });
+      return res.status(401).json({ message: "invalidCredentials" });
     }
 
     if (!accessTokenSecret) {
-      return res
-        .status(500)
-        .json({ message: "Server error - Access token secret not found." });
+      return res.status(500).json({ message: "accessTokenSecretNotFound" });
     }
 
     const accessToken = generateaccessToken(user.id, accessTokenSecret);
 
     if (!accessToken) {
-      return res
-        .status(500)
-        .json({ message: "Server error. Token could not be created." });
+      return res.status(500).json({ message: "accessTokenCreateError" });
     }
 
     // SET ACCESS TOKEN IN COOKIE
@@ -48,7 +48,7 @@ export const login = async (req, res, next) => {
     });
 
     res.status(200).json({
-      message: "Login successfull.",
+      message: "success",
       data: {
         id: user.user_id,
         email: user.email,
