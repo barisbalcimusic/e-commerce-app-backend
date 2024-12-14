f12b41c514c8b3838d01dec0cd9969e6627692dc
Baris Balci <barisbalcimusic@gmail.com>
2024-12-14 10:38:24 +0100

    authenticationMiddleware created

---
+++ src/middlewares/authenticationMiddleware.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+import { verifyAccessToken } from "../utils/jwt";
+
+dotenv.config();
+
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+
+export const authenticationMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookies["accessToken"];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "missingCookie",
+      });
+    }
+
+    if (!accessTokenSecret)
+      return res.status(500).json({
+        message: "envError",
+      });
+
+    const verification = verifyAccessToken(token, accessTokenSecret);
+
+    if (!verification) {
+      return res.status(401).json({
+        message: "tokenVerificationFailed",
+      });
+    }
+    req.user = verification;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
