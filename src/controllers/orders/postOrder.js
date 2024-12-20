d9416a69ab67b71cc1caa60600d176e532ec15df
Baris Balci <barisbalcimusic@gmail.com>
2024-12-20 10:12:13 +0100

    init query for adress updated

--- src/controllers/orders/postOrder.js
+++ src/controllers/orders/postOrder.js
@@ -2,23 +2,23 @@ import { pool } from "../../utils/config/DBconfig.js";
 
 export const postOrder = async (req, res, next) => {
   try {
-    const { id, total, order } = req.body;
+    const { adress, paymentMethod, cartItems } = req.body;
 
-    const orderDate = new Date().toISOString().slice(0, 19).replace("T", " ");
+    // const orderDate = new Date().toISOString().slice(0, 19).replace("T", " ");
 
-    const [result] = await pool.execute(
+    const [result] = await pool.query(
       "INSERT INTO orders (user_id, order_date, total_amount) VALUES (?, ?, ?)",
       [id, orderDate, total]
     );
 
-    const orderId = result.insertId;
+    // const orderId = result.insertId;
 
-    for (const product of order.orderedProducts) {
-      await pool.execute(
-        "INSERT INTO order_items (order_id, product_id, quantity, price) VALUES (?, ?, ?, ?)",
-        [orderId, product.id, product.amount, product.singlePrice]
-      );
-    }
+    // for (const product of order.orderedProducts) {
+    //   await pool.execute(
+    //     "INSERT INTO order_items (order_id, product_id, quantity, price) VALUES (?, ?, ?, ?)",
+    //     [orderId, product.id, product.amount, product.singlePrice]
+    //   );
+    // }
 
     res.status(201).json({ message: "Order received!" });
   } catch (error) {
